05ac441b574d038c4f2965edf30983fe7c33e8aa
Michel Moccand-J <michelm.gaming@gmail.com>
2024-03-10 20:45:43 +0100

    ✨ Update script.js (again)

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // modifier heure si nécessaire
 const endDate = new Date("2024-03-22T13:00:00").getTime();
 
-const countdown = setInterval(() => {
+const refreshCountdown = () => {
     const now = new Date().getTime();
     const distance = endDate - now;
+
+    // Reload when the timer is over
+    if (distance < 0) {
+        window.location.reload();
+    }
 
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -14,11 +19,7 @@ const countdown = setInterval(() => {
     document.getElementById("countdown").innerHTML += `<p>${hours}<span>H</span></p>`;
     document.getElementById("countdown").innerHTML += `<p>${minutes}<span>M</span></p>`;
     document.getElementById("countdown").innerHTML += `<p>${seconds}<span>S</span></p>`;
+}
 
-    if (distance < 0) {
-        clearInterval(countdown);
-        document.getElementById("countdown").innerHTML ="Le countdown est terminé !";
-    }
-}, 1000);
-
-
+setInterval(refreshCountdown, 1000);
+refreshCountdown();
